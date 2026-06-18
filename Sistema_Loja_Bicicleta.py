8b511d9c151f2fe8c603c6a4815b6a70f3a7d07e
joseemanuelcsilva-spec <joseemanuelcsilva@gmail.com>
2026-06-18 09:14:55 -0300

    commit

--- Sistema_Loja_Bicicleta.py
+++ Sistema_Loja_Bicicleta.py
@@ -3,25 +3,131 @@ from datetime import datetime
 import time
 resp = ' '
 # Dicionários 
-bicicletas = {
-     'b001':['caloi', 'adv', 200, 10]
-}
-capacetes = {
-     'c001':['assasa','sasasa', 90, 10]
-}
-sapatilhas = {
-     's001':['pe de pano', 'asaasa', 9, 3]
-}
-roupas = {
-     'r001':['specialized', 'GG', 10, 4]
-}
-clientes = {
-     'c001':['jose', 'silva',31313131, 313131331 ]
-}
+bicicletas = {}
+try:
+    arq_bicicletas = open("bicicletas.txt", "rt", encoding="utf-8")
+    for linha in arq_bicicletas:
+        linha = linha.strip()
+        if linha != "":
+            dados = linha.split(",")
+            codigo = dados[0]
+            marca = dados[1]
+            modelo = dados[2]
+            valor = float(dados[3])
+            quantidade = int(dados[4])
+            bicicletas[codigo] = [marca, modelo, valor, quantidade]
+    arq_bicicletas.close()
+except:
+    print("Arquivo não encontrado.")
+
+capacetes = {}
+try:
+    arq_capacetes = open("capacetes.txt", "rt", encoding="utf-8")
+    for linha in arq_capacetes:
+        linha = linha.strip()
+        if linha != "":
+            dados = linha.split(",")
+            codigo = dados[0]
+            marca = dados[1]
+            modelo = dados[2]
+            valor = float(dados[3])
+            quantidade = int(dados[4])
+            capacetes[codigo] = [marca, modelo, valor, quantidade]
+    arq_capacetes.close()
+
+except:
+    print("Arquivo de capacetes não encontrado.")
+
+sapatilhas = {}
+try:
+    arq_sapatilhas = open("sapatilhas.txt", "rt", encoding="utf-8")
+    for linha in arq_sapatilhas:
+        linha = linha.strip()
+        if linha != "":
+            dados = linha.split(",")
+            codigo = dados[0]
+            marca = dados[1]
+            modelo = dados[2]
+            valor = float(dados[3])
+            quantidade = int(dados[4])
+            sapatilhas[codigo] = [marca, modelo, valor, quantidade]
+    arq_sapatilhas.close()
+
+except:
+    print("Arquivo de sapatilhas não encontrado.")
+
+roupas = {}
+try:
+    arq_roupas = open("roupas.txt", "rt", encoding="utf-8")
+
+    for linha in arq_roupas:
+        linha = linha.strip()
+        if linha != "":
+            dados = linha.split(",")
+            codigo = dados[0]
+            marca = dados[1]
+            tamanho = dados[2]
+            valor = float(dados[3])
+            quantidade = int(dados[4])
+            roupas[codigo] = [marca, tamanho, valor, quantidade]
+    arq_roupas.close()
+
+except:
+    print("Arquivo de roupas não encontrado.")
+
+clientes = {}
+try:
+    arq_clientes = open("clientes.txt", "rt", encoding="utf-8")
+    for linha in arq_clientes:
+        linha = linha.strip()
+        if linha != "":
+            dados = linha.split(",")
+            codigo = dados[0]
+            nome = dados[1]
+            sobrenome = dados[2]
+            telefone = dados[3]
+            cpf = dados[4]
+            clientes[codigo] = [
+                nome,
+                sobrenome,
+                telefone,
+                cpf
+            ]
+
+    arq_clientes.close()
+
+except:
+    print("Arquivo de clientes não encontrado.")
+
 vendas = {}
+try:
+    arq_vendas = open("vendas.txt", "rt", encoding="utf-8")
+    for linha in arq_vendas:
+        linha = linha.strip()
+        if linha != "":
+            dados = linha.split(",")
+            cod_venda = dados[0]
+            cod_cliente = dados[1]
+            nome = dados[2]
+            produto = dados[3]
+            valor = float(dados[4])
+            pagamento = dados[5]
+            data_hora = dados[6]
+            status = dados[7]
+            vendas[cod_venda] = [
+                cod_cliente,
+                nome,
+                produto,
+                valor,
+                pagamento,
+                data_hora,
+                status
+            ]
+    arq_vendas.close()
+except:
+    print("Arquivo de vendas não encontrado.")
 
 while resp != 0:
-        os.system("clear")
         print('#'*31)
         print('##### Projeto - PedalFlow #####')
         print('#'*31)    
@@ -852,4 +958,52 @@ while resp != 0:
                 ''')
         else:
             print('OPÇÃO INVÁLIDA')
-print(vendas)
+
+# salvando bicicletas
+arq_bicicletas = open("bicicletas.txt", "wt", encoding="utf-8")
+for codigo, dados in bicicletas.items():
+    arq_bicicletas.write(
+        f"{codigo},{dados[0]},{dados[1]},{dados[2]},{dados[3]}\n"
+    )
+arq_bicicletas.close()
+
+## salvando capacetes
+arq_capacetes = open("capacetes.txt", "wt", encoding="utf-8")
+for codigo, dados in capacetes.items():
+    arq_capacetes.write(
+        f"{codigo},{dados[0]},{dados[1]},{dados[2]},{dados[3]}\n"
+    )
+arq_capacetes.close()
+
+## salvando sapatilhas
+arq_sapatilhas = open("sapatilhas.txt", "wt", encoding="utf-8")
+for codigo, dados in sapatilhas.items():
+    arq_sapatilhas.write(
+        f"{codigo},{dados[0]},{dados[1]},{dados[2]},{dados[3]}\n"
+    )
+arq_sapatilhas.close()
+
+# salvando roupas
+arq_roupas = open("roupas.txt", "wt", encoding="utf-8")
+for codigo, dados in roupas.items():
+    arq_roupas.write(
+        f"{codigo},{dados[0]},{dados[1]},{dados[2]},{dados[3]}\n"
+    )
+arq_roupas.close()
+
+#salvando clientes
+arq_clientes = open("clientes.txt", "wt", encoding="utf-8")
+for codigo, dados in clientes.items():
+    arq_clientes.write(
+        f"{codigo},{dados[0]},{dados[1]},{dados[2]},{dados[3]}\n"
+    )
+arq_clientes.close()
+
+#salvando vendass
+arq_vendas = open("vendas.txt", "wt", encoding="utf-8")
+for codigo, dados in vendas.items():
+    arq_vendas.write(
+        f"{codigo},{dados[0]},{dados[1]},{dados[2]},{dados[3]},{dados[4]},{dados[5]},{dados[6]}\n"
+    )
+arq_vendas.close()
+
